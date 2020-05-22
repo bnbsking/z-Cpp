71560480f3a9af5c6e090fa90f261b07ab9d68fa
Bobo <bnbsking@gmail.com>
2020-05-22 14:45:18 +0800

    Update struct.c

--- struct.c
+++ struct.c
@@ -34,3 +34,31 @@ int main()
     
     return 0;
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+int wheel; //4
+char* color; //16
+int* arr; //24
+}Car;
+
+void f(Car x){
+printf("%x %x %x %x \n", &x, &x.wheel, x.color, x.arr);
+}
+
+int main()
+{
+Car a;
+printf("%d \n", sizeof(a));
+
+Car b=a;
+printf("%x %x %x %x \n", &a, &a.wheel, a.color, a.arr);
+printf("%x %x %x %x \n", &b, &b.wheel, b.color, b.arr);
+f(a);
+
+return 0;
+}
+*/
