aa0c048ee1f4491bbea0d3e443cf0fdcac7a4101
Bobo <bnbsking@gmail.com>
2020-05-22 10:02:01 +0800

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int wheel;
+    char* color;
+    int* arr;
+}Car;
+
+void f(Car x){
+    printf("%x %x %x %x \n", &x, &x.wheel, &x.color, x.arr);
+}
+
+Car* g(Car* ptr){
+    ptr->wheel = 4;
+    ptr->color = "blue";
+    ptr->arr = malloc(1*sizeof(int));
+    return ptr;
+}
+
+int main()
+{
+    Car a;
+    a.arr = malloc(4*sizeof(int));
+    
+    Car b=a;
+    printf("%x %x %x %x \n", &a, &a.wheel, &a.color, a.arr);
+    printf("%x %x %x %x \n", &b, &b.wheel, &b.color, b.arr);
+    f(a);
+
+    Car* ptr = malloc(1*sizeof(Car));
+    ptr = g(ptr);
+    printf("%d %s %d", ptr->wheel, ptr->color, ptr->arr[0]);
+    
+    return 0;
+}
